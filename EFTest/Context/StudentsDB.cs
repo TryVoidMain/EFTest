4e04a108555bd45d7272776065f2d87a7a89b8db
Kosnstantin <tryvoidmain@gmail.com>
2021-12-07 22:22:54 +0300

    Добавил Nuget пакет EFCore, добавил класс контекста DB, в классе описал набор свойст типа DBSet<T> для доступа к таблицам БД

---
+++ EFTest/Context/StudentsDB.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+using EFTest.Entities;
+
+namespace EFTest.Context
+{
+    class StudentsDB : DbContext
+    {
+        public DbSet<Student> Students { get; set; }
+        public DbSet<Group> Groups { get; set; }
+
+        public StudentsDB(DbContextOptions<StudentsDB> options) : base(options) { }
+    }
+}
