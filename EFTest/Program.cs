2847e12b7a4eb15071fd1d41a35afb7cf2cb7972
Kosnstantin <tryvoidmain@gmail.com>
2021-12-08 22:55:03 +0300

    Всё переигралось на .net6 Добавил объект настроект подключения (connection_options).

--- EFTest/Program.cs
+++ EFTest/Program.cs
@@ -1,12 +1,19 @@
-﻿using System;
+﻿using Microsoft.EntityFrameworkCore;
+using EFTest.Context;
+using Microsoft.Extensions.Configuration;
 
 namespace EFTest
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
-            Console.WriteLine("Hello World!");
+            const string sql_server_connection_string = @"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=Students.Test.db";
+
+            var connection_options = new DbContextOptionsBuilder<StudentsDB>()
+                .UseSqlServer(sql_server_connection_string)
+                .Options;
+
         }
     }
 }
